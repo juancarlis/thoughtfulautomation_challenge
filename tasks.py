6a9db9f6d3731484916a788e820723b10aee22a4
Juan I. Carlis <juancarlis93@gmail.com>
2021-12-28 14:29:29 -0300

    dive into agency given in config file

--- tasks.py
+++ tasks.py
@@ -2,46 +2,72 @@ from RPA.Browser.Selenium import Selenium
 from time import sleep
 
 import pandas as pd
-import openpyxl
+import logging
+
+from config.common import config
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 browser_lib = Selenium()
 
 
 def open_the_website(url):
+    logger.info(f'Open website: {url}')
     browser_lib.open_available_browser(url)
 
 
 def click_dive_in():
+    logger.info('Click dive in button')
     button_xpath = '//*[@id="node-23"]/div/div/div/div/div/div/div/a'
     browser_lib.click_element_when_visible(button_xpath)
 
 
 def get_data():
+    logger.info('Access all agencies by xpath')
     data = browser_lib.get_webelements('//div[@class="col-sm-4 text-center noUnderline"]/div/div/div/div/a/span')
     data = [x.text for x in data]
 
+    logger.info('Clean data and return into list of tuples')
     agencies = [ele for idx, ele in enumerate(data) if idx % 2 != 0]
     amount = [ele for idx, ele in enumerate(data) if idx % 2 == 0]
 
-    data_tuples = list(zip(agencies, amount))
+    data_tuples = list(zip(amount, agencies))
 
     return data_tuples
 
 
 def creating_dataframe(data_tuples):
+    logger.info('Create dataframe from data tuples')
     df = pd.DataFrame(data_tuples, columns=['Agencies', 'Amount'])
 
     return df
 
 
-def saving_excel(df):
-    # writer = pd.ExcelWriter('output/agencies.xlsx', engine='openpyxl')
-    # workbook=writer.book
-    # worksheets=workbook.add_worksheet('Agencies')
-    
-    # df.to_excel(writer,sheet_name='Agencies', startrow=0, startcol=0, index=False)
+def saving_excel(df, filename):
+    logger.info(f'Save into excel file at {filename}')
 
-    df.to_excel('output/agencies.xlsx', sheet_name='Agencies', index=False, startrow=0, startcol=0)
+    df.to_excel(filename, sheet_name='Agencies', index=False, startrow=0, startcol=0)
+
+
+def dive_through_agency(agency_name):
+
+    data = browser_lib.get_webelements('//div[@class="col-sm-4 text-center noUnderline"]/div/div/div/div/a/span')
+
+    for elem in data: 
+        if elem.text == agency_name:
+            agency = elem
+
+    browser_lib.click_element_when_visible(agency)
+
+
+def get_full_table():
+
+    select_field = '//select[@name="investments-table-object_length"]/option[text()="All"]'
+    browser_lib.click_element_when_visible(select_field)
+
 
 # Define a main() function that calls the other functions in order:
 def main():
@@ -53,8 +79,15 @@ def main():
         sleep(2)
         df = creating_dataframe(data_tuples)
 
-        saving_excel(df)
-        
+        saving_excel(df, 'output/agencies.xlsx')
+
+        agency = config()['agency']
+        dive_through_agency(agency) 
+        sleep(5)
+
+        get_full_table()
+        sleep(5)
+
     finally:
         browser_lib.close_all_browsers()
 
